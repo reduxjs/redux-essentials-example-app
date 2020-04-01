09dd96a590192e2b5533bfb7b9d141c49687ce40
Mark Erikson <mark@isquaredsoftware.com>
2020-03-31 21:31:36 -0400

    First API calls

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+
+import { client } from './api/client'
 
 function App() {
+  const fetchMovies = async () => {
+    const movies = await client('/api/movies')
+    console.log(movies)
+  }
+
+  const fetchFailure = async () => {
+    try {
+      await client('/fakeApi/doesNotExist')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={fetchMovies}>Fetch Movies</button>
+        <button onClick={fetchFailure}>Fetch Failure</button>
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
