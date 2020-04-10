4bec220776002a8ebde1f4a765cb237c40e702d6
Mark Erikson <mark@isquaredsoftware.com>
2020-04-09 21:26:46 -0400

    Fix PostList keys

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
   const renderedPosts = posts.map((post) => (
-    <article className={'post-excerpt'}>
+    <article className="post-excerpt" key={post.id}>
       <h2>{post.title}</h2>
       <p>{post.content.substring(0, 100)}</p>
     </article>
