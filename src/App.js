12a3d5efe3a1bc90e21446e3d26bcaac86aa5d43
Mark Erikson <mark@isquaredsoftware.com>
2020-04-09 21:16:19 -0400

    Add nav and title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import { PostsList } from './features/posts/PostsList'
 function App() {
   return (
     <div className="App">
+      <nav>
+        <section>
+          <h1>Redux Quick Start Example</h1>
+        </section>
+      </nav>
       <PostsList />
     </div>
   )
