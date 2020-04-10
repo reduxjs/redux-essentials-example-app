05e62feb43a1f263bd7a94f7cbc0f26a46cfa251
Mark Erikson <mark@isquaredsoftware.com>
2020-04-09 21:31:07 -0400

    Extract Post

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+const Post = ({ post }) => (
+  <article className="post-excerpt">
+    <h2>{post.title}</h2>
+    <p>{post.content.substring(0, 100)}</p>
+  </article>
+)
+
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h2>{post.title}</h2>
-      <p>{post.content.substring(0, 100)}</p>
-    </article>
-  ))
+  const renderedPosts = posts.map((post) => <Post key={post.id} post={post} />)
 
   return (
     <section>
