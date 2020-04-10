da2744ffbd781112ec8f45e6b00333ed5241095b
Mark Erikson <mark@isquaredsoftware.com>
2020-04-09 21:40:13 -0400

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom'
 
 import { client } from './api/client'
 
@@ -7,15 +14,23 @@ import { AddPostForm } from './features/posts/AddPostForm'
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <section>
-          <h1>Redux Quick Start Example</h1>
-        </section>
-      </nav>
-      <AddPostForm />
-      <PostsList />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <section>
+            <h1>Redux Quick Start Example</h1>
+
+            <Link to="/">Posts</Link>
+            <Link to="/addPost">Add New Post</Link>
+          </section>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PostsList} />
+          <Route exact path="/addPost" component={AddPostForm} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
