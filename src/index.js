625862c94419e26e25c1ae0f578853e8dbf62cd1
Mark Erikson <mark@isquaredsoftware.com>
2020-04-19 22:03:10 -0400

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker'
 
 import './api/server'
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
