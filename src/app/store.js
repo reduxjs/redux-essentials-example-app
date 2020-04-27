fa57f80b91846e6401f6f170ec8795f3e01b8e79
Mark Erikson <mark@isquaredsoftware.com>
2020-04-26 22:36:02 -0400

    Add notifications slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,48 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+} from '@reduxjs/toolkit'
+
+import { client } from '../../api/client'
+
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async () => {
+    const response = await client.get('/fakeApi/notifications')
+    return response.notifications
+  }
+)
+
+const notificationsAdapter = createEntityAdapter({
+  // Sort reverse-chronologically
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: notificationsAdapter.getInitialState(),
+  reducers: {
+    allNotificationsRead(state, action) {
+      Object.values(state.entities).forEach((notification) => {
+        notification.read = true
+      })
+    },
+    notificationRead(state, action) {
+      const notification = state.entities[action.payload]
+      if (notification) {
+        notification.read = true
+      }
+    },
+  },
+  extraReducers: {
+    [fetchNotifications.fulfilled]: notificationsAdapter.upsertMany,
+  },
+})
+
+export const {
+  notificationRead,
+  allNotificationsRead,
+} = notificationsSlice.actions
+
+export default notificationsSlice.reducer
