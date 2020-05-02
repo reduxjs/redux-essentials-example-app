b9b6e8b6b51204221958d9f3034a91ec56c80229
Mark Erikson <mark@isquaredsoftware.com>
2020-05-01 21:20:54 -0400

    Throw errors on fetch failure

--- src/api/client.js
+++ src/api/client.js
@@ -24,6 +24,7 @@ export async function client(endpoint, { body, ...customConfig } = {}) {
     if (response.ok) {
       return data
     }
+    throw new Error(response.statusText)
   } catch (err) {
     return Promise.reject(err.message ? err.message : data)
   }
