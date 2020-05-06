73d0b80453f64a054e79c519c7683846e62b9504
Mark Erikson <mark@isquaredsoftware.com>
2020-05-05 21:23:09 -0400

    Add SinglePostPage

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
@@ -7,7 +8,10 @@ export const PostsList = () => {
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <p  className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
     </article>
   ))
 
