a2401e07e6e35fa377f89ef935570db12295c951
Mark Erikson <mark@isquaredsoftware.com>
2020-05-15 17:00:21 -0400

    Add a users slice

---
+++ src/features/users/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '0', name: 'Tianna Jenkins' },
+  { id: '1', name: 'Kevin Grant' },
+  { id: '2', name: 'Madison Price' },
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+})
+
+export default usersSlice.reducer
