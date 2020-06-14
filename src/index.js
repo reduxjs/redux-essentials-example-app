9334de394e80c2f736de856f1b2458e233aa127a
Mark Erikson <mark@isquaredsoftware.com>
2020-06-14 15:59:11 -0400

    Fetch a list of users from the server on startup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 
 import App from './App'
 import { store } from './app/store'
+import { fetchUsers } from './features/users/usersSlice'
 
 import './index.css'
 
@@ -13,6 +14,8 @@ import { worker } from './api/server'
 async function start() {
   // Start our mock API server
   await worker.start({ onUnhandledRequest: 'bypass' })
+
+  store.dispatch(fetchUsers())
 
   const root = createRoot(document.getElementById('root'))
 
