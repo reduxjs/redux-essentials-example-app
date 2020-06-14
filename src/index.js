0b8328307e3a03c5730eb49ce5be4dfafd317c95
Mark Erikson <mark@isquaredsoftware.com>
2020-06-14 15:59:11 -0400

    Fetch a list of users from the server on startup

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
 
+import { fetchUsers } from './features/users/usersSlice'
+
 // Start our mock API server
 worker.start({ onUnhandledRequest: 'bypass' })
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
