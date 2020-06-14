3befbb4881397dc96331f45d6df5a7f15c3600b0
Mark Erikson <mark@isquaredsoftware.com>
2020-06-14 15:26:36 -0400

    Extract post selectors

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -80,3 +80,8 @@ const postsSlice = createSlice({
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
+
+export const selectAllPosts = (state) => state.posts
+
+export const selectPostById = (state, postId) =>
+  state.posts.find((post) => post.id === postId)
