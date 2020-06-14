5add257bdc87e61258614e4e68455b47e9806b6a
Mark Erikson <mark@isquaredsoftware.com>
2020-06-14 15:59:11 -0400

    Fetch a list of users from the server on startup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
+import { fetchUsers } from './features/users/usersSlice'
+
 import './api/server'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
   <React.StrictMode>
