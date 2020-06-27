9ba6dd74352b436eed1022c9912ebbc256c20899
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 14:18:02 -0400

    Add notifications slice

---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { client } from '../../api/client'
+
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      `/fakeApi/notifications?since=${latestTimestamp}`
+    )
+    return response.data
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      // Sort with newest first
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
+})
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = (state) => state.notifications
