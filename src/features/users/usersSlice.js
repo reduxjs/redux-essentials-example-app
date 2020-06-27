4948955ddff76493937d4dc46163d050e2f07a15
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 14:08:40 -0400

    Add UsersList and UserPage components

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -20,3 +20,8 @@ const usersSlice = createSlice({
 })
 
 export default usersSlice.reducer
+
+export const selectAllUsers = (state) => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
