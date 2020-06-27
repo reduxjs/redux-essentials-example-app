44d6a2fe79a54c5e67076bd6dbfdc4be5e8d4126
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 14:08:40 -0400

    Add UsersList and UserPage components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { EditPostForm } from './features/posts/EditPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 function App() {
   return (
@@ -31,6 +33,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
           <Redirect to="/" />
         </Switch>
       </div>
---
+++ src/features/users/UsersList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { selectAllUsers } from './usersSlice'
+
+export const UsersList = () => {
+  const users = useSelector(selectAllUsers)
+
+  const renderedUsers = users.map((user) => (
+    <li key={user.id}>
+      <Link to={`/users/${user.id}`}>{user.name}</Link>
+    </li>
+  ))
+
+  return (
+    <section>
+      <h2>Users</h2>
+
+      <ul>{renderedUsers}</ul>
+    </section>
+  )
+}
