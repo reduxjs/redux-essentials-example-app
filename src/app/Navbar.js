89ed0b6d40bc55d6fb81792567373a1c5323f5f5
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 14:44:13 -0400

    Mark notifications as read and new

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { fetchNotifications } from '../features/notifications/notificationsSlice'
+import {
+  fetchNotifications,
+  selectAllNotifications,
+} from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter((n) => !n.read).length
 
   const fetchNewNotifications = () => {
     dispatch(fetchNotifications())
+  }
+
+  let unreadNotificationsBadge
+
+  if (numUnreadNotifications > 0) {
+    unreadNotificationsBadge = (
+      <span className="badge">{numUnreadNotifications}</span>
+    )
   }
 
   return (
@@ -20,7 +33,9 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
-            <Link to="/notifications">Notifications</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
           </div>
 
           <button className="button" onClick={fetchNewNotifications}>
