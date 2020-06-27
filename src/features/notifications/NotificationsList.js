dfe3751d6c4488bf006d8977795e560a62e71999
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 14:44:13 -0400

    Mark notifications as read and new

--- src/features/notifications/NotificationsList.js
+++ src/features/notifications/NotificationsList.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useLayoutEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { formatDistanceToNow, parseISO } from 'date-fns'
+import classnames from 'classnames'
 
 import { selectAllUsers } from '../users/usersSlice'
 
-import { selectAllNotifications } from './notificationsSlice'
+import {
+  selectAllNotifications,
+  allNotificationsRead,
+} from './notificationsSlice'
 
 export const NotificationsList = () => {
+  const dispatch = useDispatch()
   const notifications = useSelector(selectAllNotifications)
   const users = useSelector(selectAllUsers)
+
+  useLayoutEffect(() => {
+    dispatch(allNotificationsRead())
+  })
 
   const renderedNotifications = notifications.map((notification) => {
     const date = parseISO(notification.date)
@@ -17,8 +26,12 @@ export const NotificationsList = () => {
       name: 'Unknown User',
     }
 
+    const notificationClassname = classnames('notification', {
+      new: notification.isNew,
+    })
+
     return (
-      <div key={notification.id} className="notification">
+      <div key={notification.id} className={notificationClassname}>
         <div>
           <b>{user.name}</b> {notification.message}
         </div>
