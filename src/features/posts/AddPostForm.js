ff7969f345ba2ceb958ae4d1b6b05938deb204c1
Mark Erikson <mark@isquaredsoftware.com>
2020-06-27 17:03:20 -0400

    Convert usersSlice to use createEntityAdapter

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { addNewPost } from './postsSlice'
+import { selectAllUsers } from '../users/usersSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
@@ -10,7 +11,7 @@ export const AddPostForm = () => {
   const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const dispatch = useDispatch()
-  const users = useSelector((state) => state.users)
+  const users = useSelector(selectAllUsers)
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import { selectUserById } from '../users/usersSlice'
+
 export const PostAuthor = ({ userId }) => {
-  const author = useSelector((state) =>
-    state.users.find((user) => user.id === userId)
-  )
+  const author = useSelector((state) => selectUserById(state, userId))
 
   return <span>by {author ? author.name : 'Unknown author'}</span>
 }
