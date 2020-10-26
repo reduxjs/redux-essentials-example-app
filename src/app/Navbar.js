4ed111e894ae9a785e7742e12c4b1f9224ede1bf
Satnam S Virdi <satnamsvirdi@outlook.com>
2020-10-26 12:01:12 +0530

    initial commit

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from "react";
+
+import {Link} from "react-router-dom"
 
 export const Navbar = () => {
   return (
@@ -7,9 +9,11 @@ export const Navbar = () => {
         <h1>Redux Essentials Example</h1>
 
         <div className="navContent">
-          <div className="navLinks"></div>
+          <div className="navLinks">
+            <Link to="/">Posts</Link>
+          </div>
         </div>
       </section>
     </nav>
-  )
-}
+  );
+};
---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export const PostsList = () => {
+  const posts = useSelector((state) => state.posts);
+
+  const renderedPosts = posts.map((post) => (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
