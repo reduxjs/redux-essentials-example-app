ae047d31afc83474c33adc853f28b4367fde413b
tai02 <taito.yokomizo@88oct.co.jp>
2021-02-18 00:05:50 +0900

    feat: post lists

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsList } from './features/posts/PostsList'
 
 function App() {
   return (
@@ -18,9 +20,10 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
             )}
           />
           <Redirect to="/" />
---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
+  const dispatch = useDispatch()
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content
+        })
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked}>Save Post</button>
+      </form>
+    </section>
+  )
+}
