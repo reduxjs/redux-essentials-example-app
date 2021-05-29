4998a11be8ed3bbbd0b00501b9edffc52fa5f6d7
Joshua Cadavez <>
2021-05-29 00:32:50 -0400

    SinglePostPage routing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Navbar } from './app/Navbar'
 
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Redirect to="/" />
         </Switch>
       </div>
