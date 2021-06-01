f20279072c629482ae6ec2d6a45d29c1c517857e
Soeren Dolling <soeren.dolling@gmail.com>
2021-06-01 10:51:19 +0200

    Add PostsLists to App.js

--- src/features/posts/PostLists.js
+++ src/features/posts/PostLists.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-export const PostList = () => {
+export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
 
   const renderedPosts = posts.map((post) => (
