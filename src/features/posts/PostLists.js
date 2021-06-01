00b10463d4c2cf7db5f72b47763bd96006379063
Soeren Dolling <soeren.dolling@gmail.com>
2021-06-01 10:30:38 +0200

    Create PostLists.js

---
+++ src/features/posts/PostLists.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const PostList = () => {
+  const posts = useSelector((state) => state.posts)
+
+  const renderedPosts = posts.map((post) => (
+    <article className="post-exerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <p className="posts-content">{post.content.substring(0, 100)}</p>
+    </article>
+  ))
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  )
+}
