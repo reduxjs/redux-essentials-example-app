44afdb6c5cadf087f1e8411b06ab5ed1e0ccce33
Mark Erikson <mark@isquaredsoftware.com>
2021-07-24 15:31:32 -0400

    Convert users selectors to read from API cache

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,31 +1,28 @@
-import {
-  createSlice,
-  createAsyncThunk,
-  createEntityAdapter,
-} from '@reduxjs/toolkit'
-import { client } from '../../api/client'
+import { createEntityAdapter, createSelector } from '@reduxjs/toolkit'
 
+import { apiSlice } from '../api/apiSlice'
+
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+const emptyUsers = []
+
+export const selectAllUsers = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult?.data ?? emptyUsers
+)
+
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state, userId) => userId,
+  (users, userId) => users.find((user) => user.id === userId)
+)
+
+/*
 const usersAdapter = createEntityAdapter()
 
-const initialState = usersAdapter.getInitialState()
-
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const response = await client.get('/fakeApi/users')
-  return response.users
-})
-
-const usersSlice = createSlice({
-  name: 'users',
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
-  },
-})
-
-export default usersSlice.reducer
 
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
 } = usersAdapter.getSelectors((state) => state.users)
+*/
