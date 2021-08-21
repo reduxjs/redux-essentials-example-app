38d607afefec1398a94684156a7501911e5bba9f
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 13:29:04 -0400

    Add editPost mutation

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -20,6 +20,13 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: ['Post'],
     }),
+    editPost: builder.mutation({
+      query: (post) => ({
+        url: `posts/${post.id}`,
+        method: 'PATCH',
+        body: post,
+      }),
+    }),
   }),
 })
 
@@ -27,4 +34,5 @@ export const {
   useGetPostsQuery,
   useGetPostQuery,
   useAddNewPostMutation,
+  useEditPostMutation,
 } = apiSlice
