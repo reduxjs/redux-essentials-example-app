0507899b21cc53e2357356a7ed939fb8399bbc7a
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 14:09:21 -0400

    Convert users query to store normalized response data

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -6,16 +6,17 @@ import {
 
 import { apiSlice } from '../api/apiSlice'
 
-/* Temporarily ignore adapter - we'll use this again shortly
 const usersAdapter = createEntityAdapter()
 
 const initialState = usersAdapter.getInitialState()
-*/
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => '/users',
+      transformResponse: (res) => {
+        return usersAdapter.setAll(initialState, res)
+      },
     }),
   }),
 })
@@ -28,22 +29,12 @@ export const { useGetUsersQuery } = extendedApiSlice
 // In this case, the users query has no params, so we don't pass anything to select()
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select()
 
-const emptyUsers = []
-
-export const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult?.data ?? emptyUsers
+  (usersResult) => usersResult.data
 )
 
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state, userId) => userId,
-  (users, userId) => users.find((user) => user.id === userId)
-)
-
-/* Temporarily ignore selectors - we'll come back to this later
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
-} = usersAdapter.getSelectors((state) => state.users)
-*/
+} = usersAdapter.getSelectors((state) => selectUsersData(state) ?? initialState)
