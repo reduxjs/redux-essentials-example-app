41e26916d93d9469832d9c9af13bf512c0591dfd
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 13:21:28 -0400

    Show re-fetching status for PostsList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,5 +1,6 @@
 import React, { useMemo } from 'react'
 import { Link } from 'react-router-dom'
+import classnames from 'classnames'
 
 import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
@@ -30,6 +31,7 @@ export const PostsList = () => {
   const {
     data: posts = [],
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
@@ -47,9 +49,15 @@ export const PostsList = () => {
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = sortedPosts.map((post) => (
+    const renderedPosts = sortedPosts.map((post) => (
       <PostExcerpt key={post.id} post={post} />
     ))
+
+    const containerClassname = classnames('posts-container', {
+      disabled: isFetching,
+    })
+
+    content = <div className={containerClassname}>{renderedPosts}</div>
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
