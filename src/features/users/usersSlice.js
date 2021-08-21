443ac1ee48bec408a4df96e0024bca1154a6dcf6
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 13:58:15 -0400

    Convert users selectors to read from API cache

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,31 +1,39 @@
 import {
   createSlice,
-  createAsyncThunk,
   createEntityAdapter,
+  createSelector,
 } from '@reduxjs/toolkit'
-import { client } from '../../api/client'
 
+import { apiSlice } from '../api/apiSlice'
+
+/* Temporarily ignore adapter - we'll use this again shortly
 const usersAdapter = createEntityAdapter()
 
 const initialState = usersAdapter.getInitialState()
+*/
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-  const response = await client.get('/fakeApi/users')
-  return response.data
-})
+// Calling `someEndpoint.select(someArg)` generates a new selector that will return
+// the query result object for a query with those parameters.
+// To generate a selector for a specific query argument, call `select(theQueryArg)`.
+// In this case, the users query has no params, so we don't pass anything to select()
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
 
-const usersSlice = createSlice({
-  name: 'users',
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll)
-  },
-})
+const emptyUsers = []
 
-export default usersSlice.reducer
+export const selectAllUsers = createSelector(
+  selectUsersResult,
+  (usersResult) => usersResult?.data ?? emptyUsers
+)
 
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state, userId) => userId,
+  (users, userId) => users.find((user) => user.id === userId)
+)
+
+/* Temporarily ignore selectors - we'll come back to this later
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById,
 } = usersAdapter.getSelectors((state) => state.users)
+*/
