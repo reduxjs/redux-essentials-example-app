cf7147811389b334132d528a26c641fc96b955a1
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 14:34:47 -0400

    Switch UserPage to read posts from cache

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { createSelector } from '@reduxjs/toolkit'
 
 import { selectUserById } from '../users/usersSlice'
-import { selectPostsByUser } from '../posts/postsSlice'
+import { useGetPostsQuery } from '../api/apiSlice'
 
 export const UserPage = ({ match }) => {
   const { userId } = match.params
 
   const user = useSelector((state) => selectUserById(state, userId))
 
-  const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
+  const selectPostsForUser = useMemo(() => {
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res) => res.data,
+      (res, userId) => userId,
+      (data, userId) => data.filter((post) => post.user === userId)
+    )
+  }, [])
+
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (res) => ({
+      ...res,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be filtered list of posts
+      postsForUser: selectPostsForUser(res, userId),
+    }),
+  })
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
