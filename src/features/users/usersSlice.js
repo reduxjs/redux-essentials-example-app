06fc44443f8da155fa8fcb40b0f2cdc19a55f561
Mark Erikson <mark@isquaredsoftware.com>
2021-08-21 14:07:19 -0400

    Extract getUsers into an injected endpoint

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -8,11 +8,21 @@ const usersAdapter = createEntityAdapter()
 const initialState = usersAdapter.getInitialState()
 */
 
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => '/users',
+    }),
+  }),
+})
+
+export const { useGetUsersQuery } = extendedApiSlice
+
 // Calling `someEndpoint.select(someArg)` generates a new selector that will return
 // the query result object for a query with those parameters.
 // To generate a selector for a specific query argument, call `select(theQueryArg)`.
 // In this case, the users query has no params, so we don't pass anything to select()
-export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select()
 
 const emptyUsers = []
 
