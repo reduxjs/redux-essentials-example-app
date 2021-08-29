372782335269592e44a02b6c5d9a12e1a527c2bb
Mark Erikson <mark@isquaredsoftware.com>
2021-08-29 16:29:34 -0400

    Remove unused postsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import postsReducer from '../features/posts/postsSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
 import { apiSlice } from '../features/api/apiSlice'
 
 export default configureStore({
   reducer: {
-    posts: postsReducer,
     notifications: notificationsReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
