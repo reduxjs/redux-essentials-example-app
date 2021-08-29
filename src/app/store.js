60546c4ed851032060d216faabf57f4b7d0c6129
Mark Erikson <mark@isquaredsoftware.com>
2021-08-29 16:29:34 -0400

    Remove unused postsSlice
    # Conflicts:
    #	src/app/store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import postsReducer from '../features/posts/postsSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
 import { apiSlice } from '../features/api/apiSlice'
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer,
     notifications: notificationsReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
