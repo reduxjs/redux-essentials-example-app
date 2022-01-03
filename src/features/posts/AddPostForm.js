16fb79236578f044f0eb9bbc7b820d49c4dfd738
Michael Singh <singh.michaelanthony@gmail.com>
2022-01-03 18:49:08 -0500

    preparing action payloads

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -14,14 +14,7 @@ export const AddPostForm = () => {
 
     const onSavePostClicked = () => {
         if (title && content) {
-            dispatch(
-                postAdded({
-                    id: nanoid(),
-                    title,
-                    content
-                })
-            )
-
+            dispatch(postAdded(title, content))
             setTitle('')
             setContent('')
         }
