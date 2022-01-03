e4a6413813a1734fb4c37b9c08b93a7fa5f59b17
Michael Singh <singh.michaelanthony@gmail.com>
2022-01-03 18:19:09 -0500

    single post page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Navbar } from './app/Navbar'
 
 import { AddPostForm } from './features/posts/AddPostForm'
 import { PostsList } from './features/posts/PostsList'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Redirect to="/" />
         </Switch>
       </div>
