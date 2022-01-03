fc02e1f313681ab873e5b1948c55b9dc56904518
Michael Singh <singh.michaelanthony@gmail.com>
2022-01-03 18:09:46 -0500

    save post reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -8,7 +8,13 @@ const initialState = [
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {
+        postAdded(state, action) {
+            state.push(action.payload)
+        }
+    }
 })
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
