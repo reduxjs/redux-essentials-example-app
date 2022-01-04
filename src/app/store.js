d81a7a6c42ecb418a793bb57ebc36c1f4b26f6e4
Michael Singh <singh.michaelanthony@gmail.com>
2022-01-03 19:10:01 -0500

    post author

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
 export default configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer
   }
 })
