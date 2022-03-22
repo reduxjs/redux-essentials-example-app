ab922226b3056d0f03d090682d520e8edcf11bfb
Amalendhu K R <90552668+AmalendhuKR@users.noreply.github.com>
2022-03-23 02:01:19 +0530

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import postsReducer from "../features/posts/postsSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export default configureStore({
-  reducer: () => ({}),
-})
+  reducer: {
+    posts: postsReducer,
+    users: usersReducer
+  }
+});
