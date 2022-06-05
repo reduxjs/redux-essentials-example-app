8ce7d1f0f2986add6c0604f8d2f7d7cc3ca535ed
Voyager101pw <voyager101sirius@gmail.com>
2022-06-06 02:18:10 +0500

    State is fix for requests (async-logic part-5)

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,21 +1,27 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 import { sub } from 'date-fns';
 
-const initialState = [
-  { 
-    id: '1', 
-    title: 'First Post!', 
-    content: 'Hello!', 
-    date: sub(new Date(), { minutes: 10}).toISOString(),
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  },
-  { id: '2', 
-    title: 'Second Post', 
-    content: 'More text', 
-    date: sub(new Date(), { minutes: 5}).toISOString(),
-    reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
-  },
-];
+// const initialState = [
+  // { 
+    // id: '1', 
+    // title: 'First Post!', 
+    // content: 'Hello!', 
+    // date: sub(new Date(), { minutes: 10}).toISOString(),
+    // reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+  // },
+  // { id: '2', 
+    // title: 'Second Post', 
+    // content: 'More text', 
+    // date: sub(new Date(), { minutes: 5}).toISOString(),
+    // reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}
+  // },
+// ];
+
+const initialState = {
+  posts: [],
+  status: 'pending',
+  error: null,
+};
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -25,7 +31,7 @@ const postsSlice = createSlice({
     // создает функцию создания действия с тем же именем что и у редюсера.
     postAdded: {
       reducer(state, { payload }) {
-        state.push(payload) 
+        state.posts.push(payload) 
         // эти мутации преобразуются в безопасные неизменяемые обновления внутри с помощью библиотеки Immer
       },
       prepare(title, content, userId) { // это 'prepare callback', - он вызывается всякий
@@ -45,15 +51,14 @@ const postsSlice = createSlice({
     },
     reactionAdded: (state, action) => {
       const { postId, reaction } = action.payload;
-      console.log(postId, reaction)
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
       if(existingPost) {
         existingPost.reactions[reaction]++
       }
     },
     postUpdated: (state, action) => {
       const { id, title, content } = action.payload;
-      const existingPost = state.find((post) => post.id === id);
+      const existingPost = state.posts.find((post) => post.id === id);
       if (existingPost) {
         existingPost.title = title;
         existingPost.content = content;
@@ -73,6 +78,6 @@ export default postsSlice.reducer;
 
 // Пользовательские селекторы
 // stateпараметром для этих функций селектора является корневой объект состояния Redux, как и для встроенных анонимных селекторов, которые мы написали непосредственно внутри useSelector.
-export const selectAllPosts = state => state.posts;
+export const selectAllPosts = state => state.posts.posts;
 
-export const selectPostById = (state, postId) => state.posts.find(post => post.id === postId)
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
