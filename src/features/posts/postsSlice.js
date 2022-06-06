e7df943ccdf4aab22721a3c7e469b509e6f7ecca
Voyager101pw <voyager101sirius@gmail.com>
2022-06-07 02:13:34 +0500

    Added async thunk & extraReducers logic (async-logic part-5)

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -24,34 +24,46 @@ const initialState = {
   error: null,
 };
 
-// thunk - вызывает ajax запрос для получения списка сообщений из серверной части
-// // createAsyncThunk принимает два аргумента:
-// 1- Строка 'posts/fetchPosts', которая будет использоваться в качестве префикса для сгенерированных типов действий.
-// 2- Фун. обратного вызова «создатель полезной нагрузки», которая должна возвращать 
-//    Promise содержащий некоторые данные или отклоненный Promiseс ошибкой
-export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
-  const response = await client.get('/fakeApi/posts');
-  return response.data;
-})
+
+ 
+// Результат вызова createAsyncThunk одна функция fetchPosts, имеющая три свойства fetchPosts.(pending|fulfilled|failrule)
+// 1)fetchPosts - выполняет колбек функцию (из второго аргумента), но перед выполнением она вызовет внутр. свойство(функция) fetchPosts.pending() для отправки типа действия 'posts/fetchPosts/pending'
+// 2)fetchPosts.pending() 3)fetchPosts.fulfilled()  4)fetchPosts.rejected() - три функции создателя действия, используются для определения статуса выполнения thunk'a
+// и его обработки в createSlice({ extraReducers(builder): { ТУТ-> builder.addCase() } })
+
+// API: https://redux-toolkit.js.org/api/createAsyncThunk#overview
+// Гайд: https://redux.js.org/tutorials/essentials/part-5-async-logic#fetching-data-with-createasyncthunk
+// Возвращаемое значение фун. createAsyncThunk: https://redux-toolkit.js.org/api/createAsyncThunk#return-value
+
+export const fetchPosts = createAsyncThunk( // // fetchPosts- выполняет ajax запрос на сервер(имитация) для получения списка сообщений.
+  'posts/fetchPosts',
+  // 'posts/fetchPosts' - использоваться в качестве префикса для генерации трех типов действий 'posts/fetchPosts/(pending|fulfilled|failrule)'.
+  
+  async () => {
+    // колбек фун. ( или "payloadCreator"): возвращает (промис из-за наличия async | значение синхронно без async)
+    // Перед началом выполнения эта функция всегда вызывает создатель действия fetchPosts.pending() (чтобы указать о состоянии выполнения thunk'a), 
+    // который сразу будет прослушан в extraReducer
+    // Результат выполнения: (Promise c данными && вызов fetchPosts.fulfilled() | отклоненный Promiseс ошибкой && вызов fetchPosts.rejected()]
+    const response = await client.get('/fakeApi/posts');
+    return response.data;
+  },
+  // Третий аргумент options объект (необязательно).
+)
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    // Когда мы пишем postAdded функцию редюсера, createSlice автоматически 
-    // создает функцию создания действия с тем же именем что и у редюсера.
+    // Когда мы пишем postAdded функцию редюсера, createSlice автоматически создает функцию создания действия с тем-же именем, что и у редюсера.
     postAdded: {
       reducer(state, { payload }) {
-        state.posts.push(payload) 
-        // эти мутации преобразуются в безопасные неизменяемые обновления внутри с помощью библиотеки Immer
+        state.posts.push(payload) // это мутация. Она преобразуются в безопасные неизменяемые обновления за счет Immer'a
       },
-      prepare(title, content, userId) { // это 'prepare callback', - он вызывается всякий
-        // раз когда мы вызываем редьюсер выше. фун prepare как бы опережает reducer и выполняется раньше.
-        // Результат выполнения функции prepare попадает во второй аргумент (action) функции reducer выше.
-        return {
+      prepare(title, content, userId) { // или 'prepare callback', вызывается перед вызовом  reducer, чтобы подготовить данные экшена. 
+        return { // Результат выполнения функции prepare объект action, который прокидывается во второй аргумент функции reducer выше. 
           payload: {
-            id: nanoid(), // Уникальные идентификаторы и другие случайные значения должны быть введены в действие, а не вычисляться в редьюсере.
-            date: new Date().toISOString(),
+            id: nanoid(), // Уникальные идентификаторы и другие случайные значения положено вычислять до попадания в редьюсер. Редюсер должен содержать основную логику.
+            date: new Date().toISOString(), // Создание даты и её трансформация в формат ISO. (гг.мм.дд)
             title,
             content,
             user: userId,
@@ -75,6 +87,24 @@ const postsSlice = createSlice({
         existingPost.content = content;
       }
     }
+  },
+  // Обрабатывает/прослушивает три фун. (создатели действия), которые порождает функция "createAsyncThunk"
+  extraReducers(builder) { 
+    // Гайд: https://redux.js.org/tutorials/essentials/part-5-async-logic#reducers-and-loading-actions
+    builder
+      // Перед выполнением фун. fetchPosts вызывает функцию fetchPosts.pending (создатель действия), которая посылает тип действия 'posts/fetchPosts/pending'
+      // этот тип действия отлавливатся и обрабатыватся в соответ. обработчике, в нашем случе в .addCase(fetchPosts.pending, ... )
+      .addCase(fetchPosts.pending, (state, action) => { // т.е. ТУТ
+        state.status = 'loading'; 
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
   }
 });
 
