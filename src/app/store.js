9923ad2db6a5b8d535d25ce74dc5022dc9a6e45a
Voyager101pw <voyager101sirius@gmail.com>
2022-06-09 00:36:03 +0500

    Added notifications slice (performance part-6)

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from '../features/posts/postsSlice.js';
 import usersReducer from '../features/users/usersSlice.js';
+import notificationsSlice from '../features/notifications/notificationsSlice.js';
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsSlice,
   },
 })
---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,30 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/server.js';
+
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState());
+    const [latestNotification] = allNotifications;
+    const latestTimestamp = latestNotification ? latestNotification.date : '';
+    const response = await client.get(`/fakeApi/notifications?since=${latestTimestamp}`)
+    return response.data;
+  }
+)
+
+export const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchNotifications.fulfilled, (state, action) => {
+        state.push(...action.payload);
+        state.sort((a,b) => b.date.localCompare(a.date));
+      })
+  }
+})
+
+export default notificationsSlice.reducer;
+
+export const selectAllNotifications = (state) => state.notifications;
