b5d3f0272119f40f76cb4f5885baca42e0f3d59e
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-14 21:04:18 +0100

    Render EditPostForm and adjust routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
-
+import { EditPostForm } from './features/posts/EditPostForm'
 
 function App() {
   return (
@@ -29,6 +29,7 @@ function App() {
             )}
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
