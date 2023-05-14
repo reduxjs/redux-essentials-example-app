c5cf34ed54fa0cc9f71c39f2f6ea295ccd5a9b58
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-14 22:00:16 +0100

    Create PostAuthor component and import to PostsList and SinglePostPage components

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { PostAuthor } from './PostAuthor';
+
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
 
     const renderedPosts = posts.map(post => 
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
+            <PostAuthor userId={post.user} />
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className='button muted-button'>
                 View Post 
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+import { PostAuthor } from './PostAuthor';
+
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
 
@@ -21,6 +23,7 @@ export const SinglePostPage = ({ match }) => {
         <section>
             <article className="post">
                 <h2>{post.title}</h2>
+                <PostAuthor userId={post.user} />
                 <p className="post-content">{post.content}</p>
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
