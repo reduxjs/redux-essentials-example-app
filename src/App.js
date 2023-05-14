efa8376078e13556d11934ab9a02f1c304458746
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-14 19:45:38 +0100

    Add the SinglePostPage route

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import { Navbar } from './app/Navbar'
 import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Redirect to="/" />
         </Switch>
       </div>
