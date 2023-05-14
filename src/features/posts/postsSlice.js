71c43df4d8e3bdb7a4f7dbc0d56d65e0bf232745
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-14 20:41:28 +0100

    Create postUpdated reducer in postsSlice

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -11,10 +11,20 @@ const postsSlice = createSlice({
     reducers: {
         postAdded(state, action) {
             state.push(action.payload)
+        },
+        postUpdated(state, action) {
+            const { id, title, content } = action.payload
+            const existingPost = state.find(post => post.id = id)
+            if (existingPost) {
+                existingPost.title = title,
+                existingPost.content = content
+            }
         }
     }
 })
 
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, postUpdated } = postsSlice.actions;
 
 export default postsSlice.reducer;
+
+// .find returns undefined if not true
