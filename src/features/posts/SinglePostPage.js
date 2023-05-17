4813833057e1f5d797b3324eb3b771e8fc1b65da
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-17 21:57:17 +0100

    Render TimeAgo in SinglePostPageComponent

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from "./TimeAgo";
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
@@ -23,7 +24,10 @@ export const SinglePostPage = ({ match }) => {
         <section>
             <article className="post">
                 <h2>{post.title}</h2>
-                <PostAuthor userId={post.user} />
+                <div>
+                    <PostAuthor userId={post.user} />
+                    <TimeAgo timestamp={post.date}/>
+                </div>
                 <p className="post-content">{post.content}</p>
                 <Link to={`/editPost/${post.id}`} className="button">
                     Edit Post
