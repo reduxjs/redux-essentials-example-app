fdda2ccd2c7ee78e13275abd6a653a4fca4091cf
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-17 21:42:56 +0100

    Sort the PostsList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,14 +3,20 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { PostAuthor } from './PostAuthor';
+import { TimeAgo } from './TimeAgo';
 
 export const PostsList = () => {
     const posts = useSelector(state => state.posts)
 
-    const renderedPosts = posts.map(post => 
+    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => 
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
-            <PostAuthor userId={post.user} />
+            <div>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+            </div>
             <p className="post-content">{post.content.substring(0, 100)}</p>
             <Link to={`/posts/${post.id}`} className='button muted-button'>
                 View Post 
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,20 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit" 
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' }
-  ]
+    { 
+        id: '1', 
+        title: 'First Post!', 
+        content: 'Hello!',
+        date: sub(new Date(), { minutes: 10 }).toISOString()
+    },
+    { 
+        id: '2', 
+        title: 'Second Post', 
+        content: 'More text',
+        date: sub(new Date(), { minutes: 5 }).toISOString()
+    }
+]
 
 const postsSlice = createSlice({
     name: 'posts',
