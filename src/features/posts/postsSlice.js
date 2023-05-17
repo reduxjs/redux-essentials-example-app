707c4dffd88518badae5a92f3d2f4df96a957459
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-05-17 21:32:03 +0100

    Create TimeAgo component

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,6 +17,7 @@ const postsSlice = createSlice({
                 return {
                     payload: {
                         id: nanoid(),
+                        date: new Date().toISOString(),
                         title,
                         content,
                         user: userId
