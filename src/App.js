aad47c2b0de86e8ea9443d8370e445d22490b118
Doniyor Aloxanov <doniyoraloxanov@gmail.com>
2023-06-07 17:20:24 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
-} from 'react-router-dom'
+  Redirect
+} from "react-router-dom";
 
-import { Navbar } from './app/Navbar'
+import { Navbar } from "./app/Navbar";
+
+import { PostsList } from "./features/posts/PostList";
 
 function App() {
   return (
@@ -18,16 +20,16 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <PostsList />
+              </React.Fragment>
             )}
           />
           <Redirect to="/" />
         </Switch>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
