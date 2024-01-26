168f48099d40ba8b83f55fd6f93386a499f4b306
Mark Erikson <mark@isquaredsoftware.com>
2024-01-25 22:45:58 -0500

    Add a users slice

---
+++ src/features/users/usersSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface User {
+  id: string
+  name: string
+}
+
+const initialState: User[] = [
+  { id: '0', name: 'Tianna Jenkins' },
+  { id: '1', name: 'Kevin Grant' },
+  { id: '2', name: 'Madison Price' },
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+})
+
+export default usersSlice.reducer
