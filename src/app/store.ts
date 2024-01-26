a398b15463df78b535c5da5f2c1f43d928fc2b30
Mark Erikson <mark@isquaredsoftware.com>
2024-01-25 21:40:16 -0500

    Add store types and hooks

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,3 +3,9 @@ import { configureStore } from '@reduxjs/toolkit'
 export const store = configureStore({
   reducer: () => ({}),
 })
+
+// Infer the type of `store`
+export type AppStore = typeof store
+export type RootState = ReturnType<AppStore['getState']>
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = AppStore['dispatch']
