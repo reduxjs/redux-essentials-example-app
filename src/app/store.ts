475fad1ba0c658e6c3ada351153fd618667b1efe
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 21:10:21 -0500

    Add initial API slice with getPosts query

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
 import notificationsReducer from '@/features/notifications/notificationsSlice'
+import { apiSlice } from '@/features/api/apiSlice'
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware)
   },
 })
 
