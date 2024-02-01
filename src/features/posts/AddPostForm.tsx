ca864ce263ec4adbce0f523da533f74ca5794ab0
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 21:31:59 -0500

    Update AddNewPostForm to use RTKQ useAddNewPostMutation

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,19 +1,15 @@
 import React, { useState } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { useAppSelector } from '@/app/hooks'
+import { useAddNewPostMutation } from '@/features/api/apiSlice'
 import { selectAllUsers } from '@/features/users/usersSlice'
-
-import { addNewPost } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
-  const [addRequestStatus, setAddRequestStatus] = useState<'idle' | 'pending'>(
-    'idle',
-  )
+  const [addNewPost, { isLoading }] = useAddNewPostMutation()
 
-  const dispatch = useAppDispatch()
   const users = useAppSelector(selectAllUsers)
 
   const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,21 +24,17 @@ export const AddPostForm = () => {
     setUserId(e.target.value)
   }
 
-  const canSave =
-    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
+  const canSave = [title, content, userId].every(Boolean) && !isLoading
 
   const onSavePostClicked = async () => {
     if (canSave) {
       try {
-        setAddRequestStatus('pending')
-        await dispatch(addNewPost({ title, content, user: userId })).unwrap()
+        await addNewPost({ title, content, user: userId }).unwrap()
         setTitle('')
         setContent('')
         setUserId('')
       } catch (err) {
         console.error('Failed to save the post: ', err)
-      } finally {
-        setAddRequestStatus('idle')
       }
     }
   }
@@ -58,25 +50,14 @@ export const AddPostForm = () => {
       <h2>Add a New Post</h2>
       <form>
         <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChanged}
-        />
+        <input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
         <label htmlFor="postAuthor">Author:</label>
         <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
           <option value=""></option>
           {usersOptions}
         </select>
         <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
+        <textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
         <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
