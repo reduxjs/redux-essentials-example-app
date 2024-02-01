ee9a8e5732d6f9330ee2c3ee221dde0f21434d0c
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 21:46:18 -0500

    Add editPost mutation

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import type { Post, NewPost } from '@/features/posts/postsSlice'
+import type { Post, NewPost, PostUpdate } from '@/features/posts/postsSlice'
 export type { Post }
 
 export const apiSlice = createApi({
@@ -23,7 +23,14 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: ['Post'],
     }),
+    editPost: builder.mutation<Post, PostUpdate>({
+      query: (post) => ({
+        url: `posts/${post.id}`,
+        method: 'PATCH',
+        body: post,
+      }),
+    }),
   }),
 })
 
-export const { useGetPostsQuery, useGetPostQuery, useAddNewPostMutation } = apiSlice
+export const { useGetPostsQuery, useGetPostQuery, useAddNewPostMutation, useEditPostMutation } = apiSlice
