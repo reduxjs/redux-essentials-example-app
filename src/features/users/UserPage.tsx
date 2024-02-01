777457bfabc1c1313acd6e67b876825a712aab98
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 22:43:46 -0500

    Switch UserPage to read posts from cache

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { createSelector } from '@reduxjs/toolkit'
+import type { TypedUseQueryStateResult } from '@reduxjs/toolkit/query/react'
 
 import { useAppSelector } from '@/app/hooks'
-import { selectPostsByUser } from '@/features/posts/postsSlice'
 
 import { selectUserById } from './usersSlice'
+import { useGetPostsQuery, Post } from '../api/apiSlice'
+
+type GetPostSelectFromResultArg = TypedUseQueryStateResult<Post[], any, any>
 
 export const UserPage = () => {
   const { userId } = useParams()
 
   const user = useAppSelector((state) => selectUserById(state, userId!))
 
-  const postsForUser = useAppSelector((state) =>
-    selectPostsByUser(state, userId!),
-  )
+  const selectPostsForUser = useMemo(() => {
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res: GetPostSelectFromResultArg) => res.data,
+      (res: GetPostSelectFromResultArg, userId: string) => userId,
+      (data, userId) => data?.filter((post) => post.user === userId),
+    )
+  }, [])
 
-  const postTitles = postsForUser.map((post) => (
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (res) => ({
+      ...res,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be filtered list of posts
+      postsForUser: selectPostsForUser(res, userId!),
+    }),
+  })
+
+  const postTitles = postsForUser?.map((post) => (
     <li key={post.id}>
       <Link to={`/posts/${post.id}`}>{post.title}</Link>
     </li>
