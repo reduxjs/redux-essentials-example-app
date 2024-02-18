0db115999aae175e5d3a331684130c0042f9bc3d
Mark Erikson <mark@isquaredsoftware.com>
2024-02-18 16:19:07 -0500

    Use current user for AddPostForm

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -6,10 +6,9 @@ import { postAdded } from './postsSlice'
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
-  const [userId, setUserId] = useState('')
 
   const dispatch = useAppDispatch()
-  const users = useAppSelector((state) => state.users)
+  const userId = useAppSelector((state) => state.auth.username)!
 
   const onTitleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value)
@@ -17,10 +16,6 @@ export const AddPostForm = () => {
 
   const onContentChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value)
-  }
-
-  const onAuthorChanged = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setUserId(e.target.value)
   }
 
   const onSavePostClicked = () => {
@@ -34,36 +29,14 @@ export const AddPostForm = () => {
 
   const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
-  const usersOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
-
   return (
     <section>
       <h2>Add a New Post</h2>
       <form>
         <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-          <option value=""></option>
-          {usersOptions}
-        </select>
+        <input type="text" id="postTitle" name="postTitle" value={title} onChange={onTitleChanged} />
         <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
+        <textarea id="postContent" name="postContent" value={content} onChange={onContentChanged} />
         <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
