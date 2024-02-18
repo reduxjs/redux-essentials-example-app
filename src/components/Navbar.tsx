f65fcd8219c4a20a4a7a1381ee4f83a67645113a
Mark Erikson <mark@isquaredsoftware.com>
2024-02-18 17:10:03 -0500

    Handle logged-in user in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { UserIcon } from './UserIcon'
+import { userLoggedOut } from '@/features/auth/authSlice'
+
 export const Navbar = () => {
+  const dispatch = useAppDispatch()
+  const username = useAppSelector((state) => state.auth.username)
+  const user = useAppSelector((state) => state.users.find((user) => user.id === username))
+
+  const isLoggedIn = !!username && !!user
+
+  let navContent: React.ReactNode = null
+
+  if (isLoggedIn) {
+    const onLogoutClicked = () => {
+      dispatch(userLoggedOut())
+    }
+
+    navContent = (
+      <div className="navContent">
+        <div className="navLinks">
+          <Link to="/posts">Posts</Link>
+        </div>
+        <div className="userDetails">
+          <UserIcon size={32} />
+          {user.name}
+          <button className="button small" onClick={onLogoutClicked}>
+            Log Out
+          </button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
-
-        <div className="navContent">
-          <div className="navLinks">
-            <Link to="/">Posts</Link>
-          </div>
-        </div>
+        {navContent}
       </section>
     </nav>
   )
