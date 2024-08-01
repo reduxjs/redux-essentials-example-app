b977a0e575a240da4fa98c92ec8709a045fa5cfa
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Sort posts in PostsList

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Spinner } from '@/components/Spinner'
@@ -32,12 +32,18 @@ function PostExcerpt({ post }: PostExcerptProps) {
 export const PostsList = () => {
   const { data: posts = [], isLoading, isSuccess, isError, error } = useGetPostsQuery()
 
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = posts.slice()
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+    return sortedPosts
+  }, [posts])
+
   let content: React.ReactNode
 
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = posts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    content = sortedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
