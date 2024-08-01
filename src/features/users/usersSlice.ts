c96646dfcfb14b711e3a4d231e31c84fb34ef9ad
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:42 -0400

    Convert users selectors to read from API cache
    # Conflicts:
    #	src/components/Navbar.tsx
    #	src/features/auth/LoginPage.tsx
    #	src/features/users/usersSlice.ts
    
    # Conflicts:
    #	src/features/users/usersSlice.ts

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,10 +1,11 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
+import { createEntityAdapter, createSelector, createSlice } from '@reduxjs/toolkit'
 
 import { client } from '@/api/client'
 
 import type { RootState } from '@/app/store'
 import { createAppAsyncThunk } from '@/app/withTypes'
 
+import { apiSlice } from '@/features/api/apiSlice'
 import { selectCurrentUsername } from '@/features/auth/authSlice'
 
 export interface User {
@@ -32,8 +33,20 @@ const usersSlice = createSlice({
 
 export default usersSlice.reducer
 
-export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
-  (state: RootState) => state.users,
+const emptyUsers: User[] = []
+
+// Calling `someEndpoint.select(someArg)` generates a new selector that will return
+// the query result object for a query with those parameters.
+// To generate a selector for a specific query argument, call `select(theQueryArg)`.
+// In this case, the users query has no params, so we don't pass anything to select()
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+export const selectAllUsers = createSelector(selectUsersResult, (usersResult) => usersResult?.data ?? emptyUsers)
+
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state: RootState, userId: string) => userId,
+  (users, userId) => users.find((user) => user.id === userId),
 )
 
 export const selectCurrentUser = (state: RootState) => {
@@ -43,3 +56,9 @@ export const selectCurrentUser = (state: RootState) => {
   }
   return selectUserById(state, currentUsername)
 }
+
+/* Temporarily ignore adapter selectors - we'll come back to this later
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
+  (state: RootState) => state.users,
+)
+*/
