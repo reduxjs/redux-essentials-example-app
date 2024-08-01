e3af25753ab7340001ad9a3e50f2ffe7986bad66
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Extract PostExcerpt component

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -6,8 +6,28 @@ import { TimeAgo } from '@/components/TimeAgo'
 
 import { PostAuthor } from './PostAuthor'
 
-import { fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
+import { Post, fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
+
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
 
 export const PostsList = () => {
   const dispatch = useAppDispatch()
@@ -23,19 +43,7 @@ export const PostsList = () => {
   // Sort posts in reverse chronological order by datetime string
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>
-        <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <div>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </div>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <ReactionButtons post={post} />
-    </article>
-  ))
+  const renderedPosts = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
 
   return (
     <section className="posts-list">
