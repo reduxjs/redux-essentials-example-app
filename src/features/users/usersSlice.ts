068cc76ff7cf8a602ad68b0e52560c2d928acfa5
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Add a thunk to fetch users from the mock API
    # Conflicts:
    #	src/main.tsx

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,6 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+import { client } from '@/api/client'
+
 import type { RootState } from '@/app/store'
+import { createAppAsyncThunk } from '@/app/withTypes'
 
 import { selectCurrentUsername } from '@/features/auth/authSlice'
 
@@ -9,16 +12,22 @@ interface User {
   name: string
 }
 
-const initialState: User[] = [
-  { id: '0', name: 'Tianna Jenkins' },
-  { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
-]
+export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get<User[]>('/fakeApi/users')
+  return response.data
+})
+
+const initialState: User[] = []
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
