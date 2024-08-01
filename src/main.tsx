3540ea3621c777dd36431460ca99912ab5f01250
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Wrap the app in a Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import { store } from './app/store'
 
 import { worker } from './api/server'
 
@@ -17,7 +19,9 @@ async function start() {
 
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 }
