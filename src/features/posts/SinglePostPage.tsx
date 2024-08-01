f1ef79534a08e6cd5770a3d64ea2cb535773c052
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Only allow post editing for your own posts
    # Conflicts:
    #	src/features/posts/SinglePostPage.tsx

--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -3,6 +3,8 @@ import { Link, useParams } from 'react-router-dom'
 import { useAppSelector } from '@/app/hooks'
 import { TimeAgo } from '@/components/TimeAgo'
 
+import { selectCurrentUsername } from '@/features/auth/authSlice'
+
 import { PostAuthor } from './PostAuthor'
 import { selectPostById } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
@@ -11,6 +13,7 @@ export const SinglePostPage = () => {
   const { postId } = useParams()
 
   const post = useAppSelector((state) => selectPostById(state, postId!))
+  const currentUsername = useAppSelector(selectCurrentUsername)!
 
   if (!post) {
     return (
@@ -19,6 +22,8 @@ export const SinglePostPage = () => {
       </section>
     )
   }
+
+  const canEdit = currentUsername === post.user
 
   return (
     <section>
@@ -30,9 +35,11 @@ export const SinglePostPage = () => {
         </div>
         <p className="post-content">{post.content}</p>
         <ReactionButtons post={post} />
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
+        {canEdit && (
+          <Link to={`/editPost/${post.id}`} className="button">
+            Edit Post
+          </Link>
+        )}
       </article>
     </section>
   )
