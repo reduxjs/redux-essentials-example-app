e59f7ee99426373a6eef21ff2f71be7595c2f0bd
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Use prepare callback for creating post IDs
    # Conflicts:
    #	src/features/posts/AddPostForm.tsx

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { nanoid } from '@reduxjs/toolkit'
 
 import { useAppDispatch } from '@/app/hooks'
 
@@ -26,12 +25,7 @@ export const AddPostForm = () => {
     const title = elements.postTitle.value
     const content = elements.postContent.value
 
-    const newPost: Post = {
-      id: nanoid(),
-      title,
-      content,
-    }
-    dispatch(postAdded(newPost))
+    dispatch(postAdded(title, content))
 
     e.currentTarget.reset()
   }
