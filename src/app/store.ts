2d7cb1a15fe49ab054e93ad4ab3fa4d420bf6bbc
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Add posts slice
    # Conflicts:
    #	src/app/store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,26 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import type { Action } from '@reduxjs/toolkit'
 
-interface CounterState {
-  value: number
-}
-
-// An example slice reducer function that shows how a Redux reducer works inside.
-// We'll replace this soon with real app logic.
-function counterReducer(state: CounterState = { value: 0 }, action: Action) {
-  switch (action.type) {
-    // Handle actions here
-    default: {
-      return state
-    }
-  }
-}
+import postsReducer from '@/features/posts/postsSlice'
 
 export const store = configureStore({
-  // Pass in the root reducer setup as the `reducer` argument
   reducer: {
-    // Declare that `state.counter` will be updated by the `counterReducer` function
-    counter: counterReducer,
+    posts: postsReducer,
   },
 })
 
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface Post {
+  id: string
+  title: string
+  content: string
+}
+
+const initialState: Post[] = [
+  { id: '1', title: 'First Post!', content: 'Hello!' },
+  { id: '2', title: 'Second Post', content: 'More text' },
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+})
+
+export default postsSlice.reducer
