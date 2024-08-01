06444599a866b8e5ba20e5099e0b041799767985
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Add a LoginPage and require login to view posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 
+import { useAppSelector } from './app/hooks'
 import { Navbar } from './components/Navbar'
+import { LoginPage } from './features/auth/LoginPage'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
+
+import { selectCurrentUsername } from './features/auth/authSlice'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const username = useAppSelector(selectCurrentUsername)
+
+  if (!username) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -11,9 +26,19 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route path="/" element={<PostsMainPage />}></Route>
-          <Route path="/posts/:postId" element={<SinglePostPage />} />
-          <Route path="/editPost/:postId" element={<EditPostForm />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Routes>
+                  <Route path="/posts" element={<PostsMainPage />} />
+                  <Route path="/posts/:postId" element={<SinglePostPage />} />
+                  <Route path="/editPost/:postId" element={<EditPostForm />} />
+                </Routes>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
---
+++ src/features/auth/LoginPage.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useAppDispatch, useAppSelector } from '@/app/hooks'
+import { selectAllUsers } from '@/features/users/usersSlice'
+
+import { userLoggedIn } from './authSlice'
+
+interface LoginPageFormFields extends HTMLFormControlsCollection {
+  username: HTMLSelectElement
+}
+interface LoginPageFormElements extends HTMLFormElement {
+  readonly elements: LoginPageFormFields
+}
+
+export const LoginPage = () => {
+  const dispatch = useAppDispatch()
+  const users = useAppSelector(selectAllUsers)
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: React.SubmitEvent<LoginPageFormElements>) => {
+    e.preventDefault()
+
+    const username = e.currentTarget.elements.username.value
+    dispatch(userLoggedIn(username))
+    navigate('/posts')
+  }
+
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
+
+  return (
+    <section>
+      <h2>Welcome to Tweeter!</h2>
+      <h3>Please log in:</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">User:</label>
+        <select id="username" name="username" required>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <button>Log In</button>
+      </form>
+    </section>
+  )
+}
