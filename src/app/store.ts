56c63f3585fb2c6ce5d57503d985bea43cc12d2e
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Add notifications slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit'
 import authReducer from '@/features/auth/authSlice'
 import postsReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
+import notificationsReducer from '@/features/notifications/notificationsSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     posts: postsReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
 
---
+++ src/features/notifications/notificationsSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { client } from '@/api/client'
+
+import type { RootState } from '@/app/store'
+import { createAppAsyncThunk } from '@/app/withTypes'
+
+export interface ServerNotification {
+  id: string
+  date: string
+  message: string
+  user: string
+}
+
+export const fetchNotifications = createAppAsyncThunk('notifications/fetchNotifications', async (_unused, thunkApi) => {
+  const allNotifications = selectAllNotifications(thunkApi.getState())
+  const [latestNotification] = allNotifications
+  const latestTimestamp = latestNotification ? latestNotification.date : ''
+  const response = await client.get<ServerNotification[]>(`/fakeApi/notifications?since=${latestTimestamp}`)
+  return response.data
+})
+
+const initialState: ServerNotification[] = []
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      // Sort with newest first
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
+})
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = (state: RootState) => state.notifications
