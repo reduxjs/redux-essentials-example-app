66dfeccce16c13d5975e006e6b949d5a9ad3196b
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Use logged-in user in AddPostForm

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
-import { selectAllUsers } from '@/features/users/usersSlice'
+import { selectCurrentUsername } from '@/features/auth/authSlice'
 
 import { postAdded } from './postsSlice'
 
@@ -10,7 +10,6 @@ import { postAdded } from './postsSlice'
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
   postContent: HTMLTextAreaElement
-  postAuthor: HTMLSelectElement
 }
 interface AddPostFormElements extends HTMLFormElement {
   readonly elements: AddPostFormFields
@@ -18,7 +17,8 @@ interface AddPostFormElements extends HTMLFormElement {
 
 export const AddPostForm = () => {
   const dispatch = useAppDispatch()
-  const users = useAppSelector(selectAllUsers)
+
+  const userId = useAppSelector(selectCurrentUsername)!
 
   const handleSubmit = (e: React.SubmitEvent<AddPostFormElements>) => {
     // Prevent server submission
@@ -27,18 +27,11 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
-    const userId = elements.postAuthor.value
 
     dispatch(postAdded(title, content, userId))
 
     e.currentTarget.reset()
   }
-
-  const usersOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
 
   return (
     <section>
@@ -46,11 +39,6 @@ export const AddPostForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" name="postTitle" defaultValue="" required />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" name="postAuthor" required>
-          <option value=""></option>
-          {usersOptions}
-        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
