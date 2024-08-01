dd2607f8096a487feb8ce41ea9b66e79991e060a
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:41 -0400

    Add condition to skip duplicate posts fetch

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -41,10 +41,21 @@ const initialReactions: Reactions = {
   eyes: 0,
 }
 
-export const fetchPosts = createAppAsyncThunk('posts/fetchPosts', async () => {
-  const response = await client.get<Post[]>('/fakeApi/posts')
-  return response.data
-})
+export const fetchPosts = createAppAsyncThunk(
+  'posts/fetchPosts',
+  async () => {
+    const response = await client.get<Post[]>('/fakeApi/posts')
+    return response.data
+  },
+  {
+    condition(arg, thunkApi) {
+      const postsStatus = selectPostsStatus(thunkApi.getState())
+      if (postsStatus !== 'idle') {
+        return false
+      }
+    },
+  },
+)
 
 const initialState: PostsState = {
   posts: [],
