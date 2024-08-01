a2de14569f8433689c36b23ea427fc0ba49eab19
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:42 -0400

    Update EditPostForm to use useEditPostMutation

--- src/features/posts/EditPostForm.tsx
+++ src/features/posts/EditPostForm.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
-import { useAppSelector, useAppDispatch } from '@/app/hooks'
-import { postUpdated, selectPostById } from './postsSlice'
+import { Spinner } from '@/components/Spinner'
+
+import { useGetPostQuery, useEditPostMutation } from '@/features/api/apiSlice'
 
 // TS types for the input fields
 // See: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
@@ -17,9 +18,10 @@ interface EditPostFormElements extends HTMLFormElement {
 export const EditPostForm = () => {
   const { postId } = useParams()
 
-  const post = useAppSelector((state) => selectPostById(state, postId!))
+  const { data: post } = useGetPostQuery(postId!)
 
-  const dispatch = useAppDispatch()
+  const [updatePost, { isLoading }] = useEditPostMutation()
+
   const navigate = useNavigate()
 
   if (!post) {
@@ -30,7 +32,7 @@ export const EditPostForm = () => {
     )
   }
 
-  const onSavePostClicked = (e: React.SubmitEvent<EditPostFormElements>) => {
+  const onSavePostClicked = async (e: React.SubmitEvent<EditPostFormElements>) => {
     // Prevent server submission
     e.preventDefault()
 
@@ -39,21 +41,24 @@ export const EditPostForm = () => {
     const content = elements.postContent.value
 
     if (title && content) {
-      dispatch(postUpdated({ id: post.id, title, content }))
+      await updatePost({ id: post.id, title, content })
       navigate(`/posts/${postId}`)
     }
   }
+
+  const spinner = isLoading ? <Spinner text="Saving..." /> : null
 
   return (
     <section>
       <h2>Edit Post</h2>
       <form onSubmit={onSavePostClicked}>
         <label htmlFor="postTitle">Post Title:</label>
-        <input type="text" id="postTitle" name="postTitle" defaultValue={post.title} required />
+        <input type="text" id="postTitle" name="postTitle" defaultValue={post.title} required disabled={isLoading} />
         <label htmlFor="postContent">Content:</label>
-        <textarea id="postContent" name="postContent" defaultValue={post.content} required />
-        <button>Save Post</button>
+        <textarea id="postContent" name="postContent" defaultValue={post.content} required disabled={isLoading} />
+        <button disabled={isLoading}>Save Post</button>
       </form>
+      {spinner}
     </section>
   )
 }
