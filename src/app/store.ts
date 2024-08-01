db29e58b0acc57daeebdc88a0efcb1d3f35776c9
Mark Erikson <mark@isquaredsoftware.com>
2024-08-01 13:49:40 -0400

    Export store types

--- src/app/store.ts
+++ src/app/store.ts
@@ -23,3 +23,10 @@ export const store = configureStore({
     counter: counterReducer,
   },
 })
+
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = typeof store.dispatch
+// Same for the `RootState` type
+export type RootState = ReturnType<typeof store.getState>
